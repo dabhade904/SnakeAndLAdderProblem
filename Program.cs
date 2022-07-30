d37409a8dda9891927d9d79b21e4ceb57c204482
dabhade904 <dabhade904@gmail.com>
2022-07-31 00:09:28 +0530

    Added : Initialise project and added welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder problem");
+        }
+    }
+}

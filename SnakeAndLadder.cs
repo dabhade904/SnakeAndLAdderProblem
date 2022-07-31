cda0f3b07e1b9e71fe917bef9589ae0e9af9c18b
dabhade904 <dabhade904@gmail.com>
2022-07-31 23:52:06 +0530

    Added : fuctionaltiy to Repeat till the reaches the wining position

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -12,27 +12,33 @@ namespace SnakeAndLadderProblem
         public const int LADDER= 1;
         public const int SNAKE =2;
         public const int NO_PLAY = 0;
+        public const int WINING_POSITION = 100;
         public void PlayGame()
         {
-            int playerPosition = 0;
-            Random obj=new Random();
-            int diceRoll = obj.Next(1, 7);
-            int option = obj.Next(0, 3);
-
-            if(option == LADDER)
+            for(int i=1;i<=WINING_POSITION;i++)
             {
-                playerPosition += diceRoll;
-                Console.WriteLine("Ladder Position is : " + diceRoll);
+                int playerPosition = 0;
+                Random obj = new Random();
+                int diceRoll = obj.Next(1, 7);
+                int option = obj.Next(0, 3);
+                switch (option)
+                {
+                    case LADDER:
+                        playerPosition += diceRoll;
+                        break;
+                    case SNAKE:
+                        playerPosition -= diceRoll;
+                        break;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;      
+                }
+                if (playerPosition < 0)
+                {
+                    playerPosition = 0;
+                    Console.WriteLine("Final player position"+playerPosition);
+                }
             }
-            else if(option == SNAKE)
-            {
-                playerPosition = playerPosition - diceRoll;
-                Console.WriteLine("Ladder Position decrease by " + playerPosition);
-            }
-            else
-            {
-                Console.WriteLine("No Play");
-            }            
         }
     }
 }
